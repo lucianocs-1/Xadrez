a781273d4947cab68fb916d9b4d485c5ecef3f38
lucianocs-1 <confluciano@gmail.com>
2025-10-01 04:05:43 +0000

    Desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,8 @@
  int torre = 1;
     int bispo = 1;
     int rainha = 1;
+    int movimento = 1;
+    
 
     //Iniciando o jogo
     printf("#### Jogo de xadrez ### \n");
@@ -22,10 +24,19 @@
 //Movendo o a Rainha 8 casas a esquerda
 
     while(rainha <= 8){
-        printf("A rainha moveu %d casas á esquerda\n", rainha);
+        printf("A rainha movel %d casas á esquerda\n", rainha);
         rainha++;
+    }
+    printf("\n\n");
+
+    //Movimento do cavalo 
+    while(movimento--){
+        for(int i = 0; i < 2; i++){
+            printf("Movimento para cima \n");
+        }
+        printf("Movimento para a direita \n");
+        
     }
     
     return 0;
 }
-  x
